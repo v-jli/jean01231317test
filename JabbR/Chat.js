d94ae54cbbd7c11ed98700e054c6090c7239f7bc
jing li <dnpxjean@hotmail.com>
2013-01-22 21:35:17 -0800

    dd

--- JabbR/Chat.js
+++ JabbR/Chat.js
@@ -919,7 +919,7 @@
         // Initialize the ui, passing the user preferences
         ui.initialize(state.preferences);
 
-        ui.addMessage('Welcome to jean1' + originalTitle, 'notification');
+        ui.addMessage('Welcome to jean2' + originalTitle, 'notification');
         ui.addMessage('Use ? or type /? to display the FAQ and list of commands', 'notification');
 
         function initConnection() {
